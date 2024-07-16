75f1379e967d84223c889fe907bcbaf60be103af
Mohit <mohitanand8987@gmail.com>
2024-07-16 20:20:40 +0530

    Integrated groq to netflix gpt and also added searching functionality and fetch the details of that movies from TMDB API

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,24 +1,52 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import lang from "../utils/languageConstant";
 import { useRef } from "react";
 import groq from "../utils/groq";
+import { API_OPTIONS } from "../utils/constants";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+  //Search Movie in TMDB
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
   const handleGptSearchClick = async () => {
+    const gptQuery =
+      "Act as a Movie recommendation system and suggest some movies for the query : " +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma seperated like the example result given ahead. Example Result : Gadar, Sholay, Don, Golmaal, Koi mil gaya show only movies name nothing else on the output";
     console.log(searchText.current.value);
     // Make an API call to groq api and get movie result
     const result = await groq.chat.completions.create({
       messages: [
         {
           role: "user",
-          content: "Explain the importance of fast language models",
+          content: gptQuery,
         },
       ],
       model: "llama3-8b-8192",
     });
-    console.log(result.choices);
+    console.log(result?.choices[0]?.message?.content);
+    const gptMovies = result?.choices[0]?.message?.content.split(",");
+    // For each movie i will search TMDB API
+    const promiseArray = gptMovies.map((movie) => searchMovieTMDB(movie));
+    // As searchMovieTMDB is async function also it has not completed the api call  it will return array of promise like: [Promise,Promise,Promise,Promise,Promise] it will take some time to resolve
+
+    const tmdbResults = await Promise.all(promiseArray);
+    console.log(tmdbResults);
+    dispatch(
+      addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults })
+    );
   };
   return (
     <div className="pt-[7%] flex justify-center">
